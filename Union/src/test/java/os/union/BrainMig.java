a4920ee53973498e23b2e5f17dba6448ef62c7fa
slaymaker1907 <dyllongagnier@gmail.com>
2016-12-14 21:48:49 -0700

    Made size of migration controllable

--- Union/src/test/java/os/union/BrainMig.java
+++ Union/src/test/java/os/union/BrainMig.java
@@ -3,6 +3,7 @@ package os.union;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import org.apache.log4j.Logger;
 
@@ -17,17 +18,35 @@ public class BrainMig
 	{
 		private static final long serialVersionUID = 1L;
 		private volatile boolean needSend = false;
+		private double[] dummyData;
+		
+		public MigrationProgram(double megabytes)
+		{
+			int size = (int)(megabytes * 1_000_000.0 / 8.0);
+			dummyData = new double[size];
+			Random gen = new Random(8675309);
+			for(int i = 0; i < size; i++)
+				dummyData[i] = gen.nextFloat();
+		}
 		
 		@Override
 		public void feedInput(Long input)
 		{
 			needSend = true;
 		}
+		
+		// To make sure dummyData is not optimized away.
+		public double[] getDummy() {
+			return this.dummyData;
+		}
+		
+		public double getMegs() {
+			return dummyData.length / 1_000_000.0 * 8.0;
+		}
 
 		@Override
 		public boolean computationComplete()
 		{
-			// TODO Auto-generated method stub
 			return false;
 		}
 
@@ -44,9 +63,13 @@ public class BrainMig
 		
 	public static class MigrationDetector implements PauseableProgram<Long, Long>
 	{
-		private MigrationProgram program = new MigrationProgram();
+		private MigrationProgram program;
 		private RemoteInput<Long> stdin;
 		private long startTime;
+		
+		public MigrationDetector(MigrationProgram program) {
+			this.program = program;
+		}
 		
 		@Override
 		public Program<Long, Long> getProgram()
@@ -84,8 +107,15 @@ public class BrainMig
 	
 	public static void main(String[] args) throws Exception
 	{
+		double megs;
+		if (args.length == 0)
+			megs = 100.0;
+		else
+			megs = Double.parseDouble(args[0]);
+		LOG.info("Preparing to send message of size: " + megs);
+		
 		LOG.info("Initializing brain.");
-		List<NetLocation> locations = Arrays.asList(new NetLocation("127.0.0.1", 9001), new NetLocation("127.0.0.1", 9002));
+		List<NetLocation> locations = Arrays.asList(new NetLocation("172.31.30.162", 9001), new NetLocation("172.31.23.125", 9001));
 		NetLocation brainLocation = new NetLocation("127.0.0.1", 9000);
 		WorkerManager man = new WorkerManager(locations, brainLocation);
 		try(BrainServer server = new BrainServer(9000, man))
@@ -94,7 +124,7 @@ public class BrainMig
 			LOG.info("Brain fully operational. Waiting for migration command.");
 			try(Distributer dist = new Distributer(brainLocation))
 			{
-				MigrationDetector detector = new MigrationDetector();
+				MigrationDetector detector = new MigrationDetector(new MigrationProgram(megs));
 				Thread th = new Thread(() -> 
 				{
 					try
@@ -122,5 +152,7 @@ public class BrainMig
 				Thread.sleep(100_000);
 			}
 		}
+		
+		System.exit(0);
 	}
 }
